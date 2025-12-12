59ab61b71f645bf766c3015a7747f9b31f8cce34
jeffBrasil <jefferson.ribeiro.brasil@ccc.ufcg.edu.br>
2025-12-12 14:49:28 -0300

    Correção de alguns bugs para a rota catalogo_router

--- src/tests/test_admin_auth.py
+++ src/tests/test_admin_auth.py
@@ -2,28 +2,28 @@ from fastapi.testclient import TestClient
 from fastapi import status
 
 
-admin1 = {
+admin_valido = {
     "nome": "Admin Teste",
-    "cpf": "12345678901",
-    "senha": "SenhaSegura123",
+    "cpf": "08601441009",
+    "senha": "Gri90p2M@8(Y",
 }
 
 def test_create_admin(client: TestClient):
 
-    rota = client.post("admin/criar_conta", json=admin1)
+    rota = client.post("admin/criar_conta", json=admin_valido)
 
     assert rota.status_code == status.HTTP_201_CREATED
 
     data = rota.json()
     assert "id" in data
-    assert data["cpf"] == admin1["cpf"]
-    assert data["nome"] == admin1["nome"]
+    assert data["cpf"] == admin_valido["cpf"]
+    assert data["nome"] == admin_valido["nome"]
     assert "senha" not in data
 
 def test_criar_admin_cpf_iguais(client: TestClient):
 
-    response = client.post("admin/criar_conta", json=admin1)
-    response2 = client.post("admin/criar_conta", json=admin1)
+    response = client.post("admin/criar_conta", json=admin_valido)
+    response2 = client.post("admin/criar_conta", json=admin_valido)
 
     assert response.status_code == status.HTTP_201_CREATED
     assert response2.status_code == status.HTTP_400_BAD_REQUEST
@@ -33,11 +33,11 @@ def test_criar_admin_cpf_diferentes(client: TestClient):
     admin2 = {
         "nome" : "João",
         'cpf' : "12345678910",
-        "senha" : "123456",
+        "senha" : "A144oEi%w[",
     }
 
     response = client.post("admin/criar_conta", json = admin2)
-    response2 = client.post("admin/criar_conta", json = admin1)
+    response2 = client.post("admin/criar_conta", json = admin_valido)
 
     assert(response.status_code == status.HTTP_201_CREATED)
     assert(response2.status_code == status.HTTP_201_CREATED)
