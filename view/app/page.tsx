871ec127ffe676b2eab01f56042475b884cb2b99
PlayzyARCH <carloseliasf146@gmail.com>
2025-12-11 21:16:44 -0300

    criação da tela de cadastrar usuário

--- view/app/page.tsx
+++ view/app/page.tsx
@@ -1,65 +1,70 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
+export default function CadastroForm() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="font-sv font-normal w-screen h-screen grid grid-rows-12 grid-cols-18 bg-zinc-950">
+      <div className="bg-zinc-800 flex justify-center items-center row-span-1 col-span-18">
+
+      </div>
+
+      <div className="w-full h-full p-4 bg-zinc-200 row-start-4 row-end-11 col-start-11 col-end-17">
+        <div className="text-center text-4xl">Cadastrar um Usuario</div>
+
+        <div>
+          <form className="flex flex-col gap-4">
+
+            <div className="text-2xl flex flex-col gap-2">
+              <label className="text-2xl" htmlFor="cpf">CPF</label>
+              <input
+                id="cpf"
+                type="text"
+                placeholder="Digite seu CPF"
+                className="px-2 text-xl bg-zinc-800 text-zinc-200"
+              />
+            </div>
+
+            <div className="text-2xl flex flex-col gap-2">
+              <label className="text-2xl" htmlFor="e-mail">E-mail</label>
+              <input
+                id="e-mail"
+                type="email"
+                placeholder="Digite seu e-mail"
+                className="px-2 text-xl bg-zinc-800 text-zinc-200"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label className="text-xl" htmlFor="senha">Senha</label>
+              <input
+                id="senha"
+                type="password"
+                placeholder="Digite sua senha"
+                className="px-2 text-xl bg-zinc-800 text-zinc-200"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label className="text-2xl" htmlFor="confirmar">Confirmar Senha</label>
+              <input
+                id="confirmar"
+                type="password"
+                placeholder="Confirme sua senha"
+                className="px-2 text-2xl bg-zinc-800 text-zinc-200"
+              />
+            </div>
+
+            <Button className="bg-zinc-900 text-slate-200 mt-2 w-full">
+              Cadastrar
+            </Button>
+
+          </form>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
