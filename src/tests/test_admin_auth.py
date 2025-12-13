b481867258eb406dbee65a51bd2aa4638a7a5aa7
jeffBrasil <jefferson.ribeiro.brasil@ccc.ufcg.edu.br>
2025-12-12 22:32:55 -0300

    Finalização da classe de test admin_auth

--- src/tests/test_admin_auth.py
+++ src/tests/test_admin_auth.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 from fastapi import status
-
+from main.models.user import Super_usuario
+from sqlalchemy.orm import Session
 
 admin_valido = {
     "nome": "Admin Teste",
@@ -42,36 +43,47 @@ def test_criar_admin_cpf_diferentes(client: TestClient):
     assert(response.status_code == status.HTTP_201_CREATED)
     assert(response2.status_code == status.HTTP_201_CREATED)
 
-def test_criar_admin_senha_menor_8_digitos():
-    admin_senha_pequena{
+def test_criar_admin_senha_menor_8_digitos(client: TestClient):
+    admin_senha_pequena = {
         "cpf" : "24785993090",
         "nome" : "teste",
         "senha" : "1234567"
     }
-    response = cliente.post("admin/criar_conta", json = admin_senha_pequena)
+    response = client.post("admin/criar_conta", json = admin_senha_pequena)
     assert(response.status_code == status.HTTP_400_BAD_REQUEST)
 
-def test_criar_admin_senha_8_digitos_fraca():
+def test_criar_admin_senha_8_digitos_fraca(client: TestClient):
 
-    admin_senha_fraca{
+    admin_senha_fraca = {
         "nome" : "senha fraca",
         "cpf" : "24785993090",
         "senha" : "12345678",
     }
 
-    response = cliente.post("admin/criar_conta" , json = admin_senha_fraca)
+    response = client.post("admin/criar_conta" , json = admin_senha_fraca)
     assert(response.status_code == status.HTTP_400_BAD_REQUEST)
 
-def test_criar_admin_vazio():
-    admin_nulo {}
-    response = cliente.post("admin/criar_conta", json = admin_nulo)
+def test_criar_admin_vazio(client: TestClient):
+    admin_nulo = {}
+    response = client.post("admin/criar_conta", json = admin_nulo)
 
-    assert(response.status_code == status.HTTP_400_BAD_REQUEST)
+    assert(response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT)
 
-def test_criar_admin_sem_campos_obrigatorios():
-    admin{
+def test_criar_admin_sem_campos_obrigatorios(client: TestClient):
+    admin = {
         "nome" : "pikachu"
     }
 
-    response = client.post("admin/criar_admin", json = admin)
-    assert(response.status_code == statu.HTTP_400_BAD_REQUEST)
+    response = client.post("admin/criar_conta", json = admin)
+    assert(response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT)
+
+def test_verificar_polimorfismo(client: TestClient):
+
+    response = client.post("admin/criar_conta", json = admin_valido)
+    print(response)
+    response = response.json()
+
+    assert (response is not None)
+    assert (response["tipo_usuario"] == "admin")
+
+    
