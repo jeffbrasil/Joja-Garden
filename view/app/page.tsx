4ea550583db40cff7e73df5f5a096e1d8aaf4ad5
Carlos Elias <carloseliasf146@gmail.com>
2025-12-15 20:28:43 -0300

    updates

--- view/app/page.tsx
+++ view/app/page.tsx
@@ -7,61 +7,53 @@ import { Button } from "@/components/ui/button";
 
 export default function CadastroForm() {
   return (
-    <div className="font-sv font-normal w-screen h-screen grid grid-rows-12 grid-cols-18 bg-zinc-950">
-      <div className="bg-zinc-800 flex justify-center items-center row-span-1 col-span-18">
-
-      </div>
-
-      <div className="w-full h-full p-4 bg-zinc-200 row-start-4 row-end-11 col-start-11 col-end-17">
-        <div className="text-center text-4xl">Cadastrar um Usuario</div>
+    <div className="font-poppins text-slate-900 w-screen h-screen flex justify-center items-center bg-emerald-700">
+      <div className="w-2/8 h-2/4 rounded-md text-md flex flex-col gap-5 px-6 py-8 bg-lime-200 shadow-lg shadow-teal-950">
+        <div className="text-center font-semibold text-xl">
+          Cadastrar um Usuario
+        </div>
 
         <div>
           <form className="flex flex-col gap-4">
-
-            <div className="text-2xl flex flex-col gap-2">
-              <label className="text-2xl" htmlFor="cpf">CPF</label>
+            <div className="flex flex-col gap-2">
               <input
                 id="cpf"
                 type="text"
                 placeholder="Digite seu CPF"
-                className="px-2 text-xl bg-zinc-800 text-zinc-200"
+                className="input"
               />
             </div>
 
-            <div className="text-2xl flex flex-col gap-2">
-              <label className="text-2xl" htmlFor="e-mail">E-mail</label>
+            <div className="flex flex-col gap-2">
               <input
                 id="e-mail"
                 type="email"
                 placeholder="Digite seu e-mail"
-                className="px-2 text-xl bg-zinc-800 text-zinc-200"
+                className="input"
               />
             </div>
 
             <div className="flex flex-col gap-2">
-              <label className="text-xl" htmlFor="senha">Senha</label>
               <input
                 id="senha"
                 type="password"
                 placeholder="Digite sua senha"
-                className="px-2 text-xl bg-zinc-800 text-zinc-200"
+                className="input"
               />
             </div>
 
             <div className="flex flex-col gap-2">
-              <label className="text-2xl" htmlFor="confirmar">Confirmar Senha</label>
               <input
                 id="confirmar"
                 type="password"
                 placeholder="Confirme sua senha"
-                className="px-2 text-2xl bg-zinc-800 text-zinc-200"
+                className="input"
               />
             </div>
 
-            <Button className="bg-zinc-900 text-slate-200 mt-2 w-full">
+            <Button className="bg-teal-900 text-slate-200 mt-4 w-full h-12 hover:bg-teal-800 hover:scale-104 transition focus:scale-96 hover:shadow-sm hover:shadow-teal-700 font-semibold py-2 justify-center items-center rounded-sm">
               Cadastrar
             </Button>
-
           </form>
         </div>
       </div>
