b601697a932737030cadd3676a806abcdf9487ee
Carlos Elias <carloseliasf146@gmail.com>
2025-12-26 13:19:41 -0300

    design da tela de cadastro para administradores

---
+++ view/app/sign-up-user/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <div className="h-screen w-screen bg-zinc-950 justify-center items-center flex">
+        {children}
+      </div>
+  );
+}
